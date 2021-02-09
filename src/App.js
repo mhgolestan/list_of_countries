d5008bad38b2d316a62a240c4c3ad700f907c739
Mohammad Hossein Golestan <mohammad.h.golestan@ntnu.no>
2021-02-09 14:35:20 +0100

    Card and input box styled by material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import CountriesList from "./components/CountriesList";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
 
@@ -22,8 +35,15 @@ const App = () => {
 
   return (
     <div>
-      Find countries
-      <input value={filteredCountries} onChange={handleFilteredCountries} />
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          value={filteredCountries}
+          onChange={handleFilteredCountries}
+          id="outlined-basic"
+          label="Country"
+          variant="outlined"
+        />
+      </form>
       <CountriesList
         countriesToShow={countriesToShow}
         setFilteredCountries={setFilteredCountries}
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,7 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Weather from "./Weather";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    spacing: 20,
+    height: 140,
+  },
+}));
 
 const CountriesList = ({ countriesToShow, setFilteredCountries }) => {
+  const classes = useStyles();
+
   const handleClick = (countryName) => {
     setFilteredCountries(countryName);
   };
@@ -9,8 +29,6 @@ const CountriesList = ({ countriesToShow, setFilteredCountries }) => {
   if (countriesToShow.length === 250) {
     return <div>Insert the name of the country</div>;
   } else if (countriesToShow.length === 1) {
-    // console.log(typeof countriesToShow[0].flag);
-
     return (
       <div>
         <h1>{countriesToShow[0].name}</h1>
@@ -29,16 +47,37 @@ const CountriesList = ({ countriesToShow, setFilteredCountries }) => {
   } else if (countriesToShow.length > 10) {
     return <p>{"Too many matches"}</p>;
   } else {
+    console.log(countriesToShow);
     return (
       <div>
-        <ul>
-          {countriesToShow.map((country, i) => (
-            <li key={i}>
-              {country.name}{" "}
-              {<button onClick={() => handleClick(country.name)}>Show</button>}
-            </li>
-          ))}
-        </ul>
+        {countriesToShow.map((country, i) => (
+          <Card className={classes.root} key={i}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={country.flag}
+                title="?"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {country.name}
+                </Typography>
+                {/*<Typography variant="body2" color="textSecondary" component="p">
+                  {`dscds`}
+        </Typography>*/}
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                size="small"
+                color="primary"
+                onClick={() => handleClick(country.name)}
+              >
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
       </div>
     );
   }
